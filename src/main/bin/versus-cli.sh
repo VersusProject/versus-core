47558922412ceff1c342382fca8365aedd68279f
Luigi Marini <lmarini@ncsa.illinois.edu>
2011-03-14 21:02:15 +0000

    Maven bin assembly to build a binary release zip.
    git-svn-id: file:///home/kooper/svn2git/repo/trunk@271 1d209ac1-a921-4c1d-9267-735cbf3ade97

---
+++ src/main/bin/versus-cli.sh
@@ -0,0 +1,1 @@
+java -Dlog4j.configuration=file:../lib/log4j.properties -classpath ../lib/\* edu.illinois.ncsa.versus.cli.VersusCLI $1 $2 $3 $4 $5
---
+++ src/main/bin/versus-gui.sh
@@ -0,0 +1,1 @@
+java -Dlog4j.configuration=file:../lib/log4j.properties -classpath ../lib/\* edu.illinois.ncsa.versus.gui.VersusTestApp
