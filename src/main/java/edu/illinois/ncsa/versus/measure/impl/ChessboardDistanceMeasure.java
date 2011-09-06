7b1f99261208ab81c5fb07b8f073b4d503f91d31
Michal Ondrejcek <mondrejc@ncsa.illinois.edu>
2011-09-06 21:31:00 +0000

    git-svn-id: file:///home/kooper/svn2git/repo/trunk@300 1d209ac1-a921-4c1d-9267-735cbf3ade97

---
+++ src/main/java/edu/illinois/ncsa/versus/measure/impl/ChessboardDistanceMeasure.java
@@ -0,0 +1,168 @@
+package edu.illinois.ncsa.versus.measure.impl;
+
+import edu.illinois.ncsa.versus.UnsupportedTypeException;
+import edu.illinois.ncsa.versus.descriptor.Descriptor;
+import edu.illinois.ncsa.versus.descriptor.impl.DoubleArrayFeature;
+import edu.illinois.ncsa.versus.descriptor.impl.ThreeDimensionalDoubleArrayFeature;
+import edu.illinois.ncsa.versus.descriptor.impl.VectorFeature;
+import edu.illinois.ncsa.versus.measure.Measure;
+import edu.illinois.ncsa.versus.measure.Similarity;
+import edu.illinois.ncsa.versus.measure.SimilarityNumber;
+import edu.illinois.ncsa.versus.measure.SimilarityPercentage;
+
+/**
+ * Chessboard (Chebyshev or Maximum value) distance of two vector features.
+ * 
+ * @author Michal Ondrejcek
+ * 
+ */
+public class ChessboardDistanceMeasure implements Measure {
+	/**
+	 * Compare two arrays of the same length.
+	 * 
+	 * @param feature1
+	 *            an array
+	 * @param feature2
+	 *            another array
+	 * @return the Chessboard distance between the two given arrays
+	 * @throws Exception
+	 */
+	private SimilarityNumber compare(DoubleArrayFeature feature1,
+			DoubleArrayFeature feature2) throws Exception {
+		double distanceMax = 0;
+		double valueTmp = 0;
+
+		// Check for same length
+		if (feature1.getLength() != feature2.getLength()) {
+			throw new Exception("Features must have the same length");
+		}
+
+		for (int i = 0; i < feature1.getLength(); i++) {
+			valueTmp = Math.abs(feature1.getValue(i) - feature2.getValue(i));
+			distanceMax = Math.max(valueTmp,distanceMax);
+		}
+
+		return new SimilarityNumber(distanceMax);
+	}
+
+	/**
+	 * Compare two arrays of the same length.
+	 * 
+	 * @param feature1
+	 *            an array
+	 * @param feature2
+	 *            another array
+	 * @return the Chessboard distance between the two given arrays
+	 * @throws Exception
+	 */
+	private SimilarityNumber compare(VectorFeature feature1, VectorFeature feature2) throws Exception {
+		double distanceMax = 0;
+		double valueTmp = 0;
+
+		// Check for same length
+		if (feature1.size() != feature2.size()) {
+			throw new Exception("Features must have the same length");
+		}
+
+		for (int i = 0; i < feature1.size(); i++) {
+			if (!(feature1.get(i) instanceof double[]) || !(feature2.get(i) instanceof double[])) {
+				throw new Exception("Features must be double[]");
+			}
+			double[] arr1 = (double[])feature1.get(i);
+			double[] arr2 = (double[])feature2.get(i);
+			if (arr1.length != arr2.length) {
+				throw new Exception("Features.array must have the same length");
+			}
+			for(int j=0; j<arr1.length; j++) {
+				valueTmp = Math.abs(arr1[j] - arr2[j]);
+				distanceMax = Math.max(valueTmp,distanceMax);
+		
+			}
+		}
+		
+		return new SimilarityNumber(distanceMax);
+	}
+
+	/**
+	 * Compare two arrays of pixels of the same size.
+	 * 
+	 * @param feature1
+	 * @param feature2
+	 * @return
+	 * @throws Exception
+	 */
+	private SimilarityNumber compare(
+			ThreeDimensionalDoubleArrayFeature feature1,
+			ThreeDimensionalDoubleArrayFeature feature2) throws Exception {
+
+		// check for same height
+		if (feature1.getHeight() != feature2.getHeight()) {
+			throw new Exception("Features must have the same height");
+		}
+		// check for same width
+		if (feature1.getWidth() != feature2.getWidth()) {
+			throw new Exception("Features must have the same width");
+		}
+		// check for same number of bands
+		if (feature1.getNumBands() != feature2.getNumBands()) {
+			throw new Exception("Features must have the same width");
+		}
+
+		double distanceMax = 0;
+		double valueTmp = 0;
+
+		for (int row = 0; row < feature1.getHeight(); row++) {
+			for (int col = 0; col < feature1.getWidth(); col++) {
+				for (int band = 0; band < feature1.getNumBands(); band++) {
+					valueTmp = Math.abs(feature1.getValue(row, col, band)
+							- feature2.getValue(row, col, band));
+					distanceMax = Math.max(valueTmp,distanceMax);
+				}
+			}
+		}
+
+		return new SimilarityNumber(distanceMax);
+	}
+
+	@Override
+	public SimilarityPercentage normalize(Similarity similarity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFeatureType() {
+		return ThreeDimensionalDoubleArrayFeature.class.getName();
+	}
+
+	@Override
+	public String getName() {
+		return "Chessboard Distance";
+	}
+
+	@Override
+	public Similarity compare(Descriptor feature1, Descriptor feature2)
+			throws Exception {
+		if (feature1 instanceof DoubleArrayFeature
+				&& feature2 instanceof DoubleArrayFeature) {
+			return compare((DoubleArrayFeature) feature1,
+					(DoubleArrayFeature) feature2);
+		} else if (feature1 instanceof VectorFeature
+				&& feature2 instanceof VectorFeature) {
+			return compare((VectorFeature)feature1, (VectorFeature)feature2);
+		} else if (feature1 instanceof ThreeDimensionalDoubleArrayFeature
+				&& feature2 instanceof ThreeDimensionalDoubleArrayFeature) {
+			ThreeDimensionalDoubleArrayFeature arrayFeature1 = (ThreeDimensionalDoubleArrayFeature) feature1;
+			ThreeDimensionalDoubleArrayFeature arrayFeature2 = (ThreeDimensionalDoubleArrayFeature) feature2;
+
+			return compare(arrayFeature1, arrayFeature2);
+		} else {
+			throw new UnsupportedTypeException();
+		}
+	}
+
+	@Override
+	public Class<ChessboardDistanceMeasure> getType() {
+		return ChessboardDistanceMeasure.class;
+	}
+}
