ab51b4415b3119b636611f989b5d3219e87d1eb9
Antoine Vandecreme <antoine.vandecreme@nist.gov>
2012-11-05 13:16:07 -0500

    Reduce histograms adapters memory consumption

--- src/main/java/edu/illinois/ncsa/versus/adapter/HasRGBPixels.java
+++ src/main/java/edu/illinois/ncsa/versus/adapter/HasRGBPixels.java
@@ -1,75 +1,90 @@
-/*******************************************************************************
- * Copyright (c) 2010 University of Illinois. All rights reserved.
- * 
- * Developed by: 
- * National Center for Supercomputing Applications (NCSA)
- * University of Illinois
- * http://www.ncsa.illinois.edu/
- * 
- * Permission is hereby granted, free of charge, to any person obtaining 
- * a copy of this software and associated documentation files (the 
- * "Software"), to deal with the Software without restriction, including 
- * without limitation the rights to use, copy, modify, merge, publish, 
- * distribute, sublicense, and/or sell copies of the Software, and to permit 
- * persons to whom the Software is furnished to do so, subject to the 
- * following conditions:
- * 
- * - Redistributions of source code must retain the above copyright notice, 
- *   this list of conditions and the following disclaimers.
- * - Redistributions in binary form must reproduce the above copyright notice, 
- *   this list of conditions and the following disclaimers in the documentation 
- *   and/or other materials provided with the distribution.
- * - Neither the names of National Center for Supercomputing Applications,
- *   University of Illinois, nor the names of its contributors may 
- *   be used to endorse or promote products derived from this Software 
- *   without specific prior written permission.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
- * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- * SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
- ******************************************************************************/
 /**
- * 
+ * *****************************************************************************
+ * Copyright (c) 2010 University of Illinois. All rights reserved.
+ *
+ * Developed by: National Center for Supercomputing Applications (NCSA)
+ * University of Illinois http://www.ncsa.illinois.edu/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimers. - Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimers in the documentation and/or other materials
+ * provided with the distribution. - Neither the names of National Center for
+ * Supercomputing Applications, University of Illinois, nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * Software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH
+ * THE SOFTWARE.
+ *****************************************************************************
+ */
+/**
+ *
  */
 package edu.illinois.ncsa.versus.adapter;
 
 /**
  * A multimedia object that has pixels in RGB space. First dimension is rows,
  * second is columns, third is bands.
- * 
+ *
  * @author Luigi Marini
- * 
+ *
  */
 public interface HasRGBPixels extends Adapter {
 
-	/**
-	 * Get all pixels in document.
-	 * 
-	 * @return First dimension is rows, second is columns, third is bands
-	 */
-	double[][][] getRGBPixels();
+    /**
+     * Get all pixels in document.
+     *
+     * @return First dimension is rows, second is columns, third is bands
+     */
+    double[][][] getRGBPixels();
 
-	/**
-	 * Get one pixel at a time.
-	 * 
-	 * @param row
-	 *            the row in the image - top to bottom
-	 * @param column
-	 *            column in the image - left to right
-	 * @param band
-	 *            pixel's band
-	 * @return value of the band for the pixel
-	 */
-	double getRGBPixel(int row, int column, int band);
-    
+    /**
+     * Get one pixel at a time.
+     *
+     * @param row the row in the image - top to bottom
+     * @param column column in the image - left to right
+     * @param band pixel's band
+     * @return value of the band for the pixel
+     */
+    double getRGBPixel(int row, int column, int band);
+
+    /**
+     * Get the width of the image
+     *
+     * @return the width of the image
+     */
+    int getWidth();
+
+    /**
+     * Get the height of the image
+     *
+     * @return the height of the image
+     */
+    int getHeight();
+
     /**
      * Get the number of bits per pixel.
-     * 
+     *
      * @return the number of bits per pixel.
      */
     int getBitsPerPixel();
+    
+    /**
+     * Get the number of bands
+     * @return the number of bands
+     */
+    int getNumBands();
 }
