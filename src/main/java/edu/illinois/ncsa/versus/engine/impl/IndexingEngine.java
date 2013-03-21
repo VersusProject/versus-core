0c7f7bb089464b378498e84f4714a0806628e436
smpadhy <spadhy@illinois.edu>
2013-03-21 09:32:22 -0500

    added queryIndex method, added constructor with different parameters

--- src/main/java/edu/illinois/ncsa/versus/engine/impl/IndexingEngine.java
+++ src/main/java/edu/illinois/ncsa/versus/engine/impl/IndexingEngine.java
@@ -4,6 +4,7 @@
 package edu.illinois.ncsa.versus.engine.impl;
 
 import java.io.File;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,7 +16,9 @@ import org.apache.commons.logging.LogFactory;
 import edu.illinois.ncsa.versus.adapter.Adapter;
 import edu.illinois.ncsa.versus.descriptor.Descriptor;
 import edu.illinois.ncsa.versus.extract.Extractor;
+import edu.illinois.ncsa.versus.measure.Measure;
 import edu.illinois.ncsa.versus.search.Indexer;
+import edu.illinois.ncsa.versus.search.SearchResult;
 
 /**
  * A very simple multithreaded engine to build an index.
@@ -27,19 +30,42 @@ public class IndexingEngine {
 
 	private final ExecutorService threadPool;
 	private static Log log = LogFactory.getLog(IndexingEngine.class);
-	private final Adapter adapter;
-	private final Extractor extractor;
-	private final Indexer indexer;
-	private int numDocs = 0;
+	
+	private  Adapter adapter;
+	private  Extractor extractor;
+	private Measure measure;
 
-	public IndexingEngine(Adapter adapter, Extractor extractor,
-			Indexer indexer, int numThreads) {
-		this.adapter = adapter;
-		this.extractor = extractor;
+	private Indexer indexer;
+	private int numDocs = 0;
+	
+	public IndexingEngine(Indexer indexer, int numThreads) { 
 		this.indexer = indexer;
 		threadPool = Executors.newFixedThreadPool(numThreads);
 	}
-
+	
+	public IndexingEngine(int numThreads){
+		threadPool = Executors.newFixedThreadPool(numThreads);
+		
+	}
+	public void setAdapter(Adapter adapter){
+		this.adapter=adapter;
+	  }
+	
+	public void setExtractor(Extractor extractor){
+		
+	   this.extractor=extractor;
+		
+	}
+	
+	public void setMeasure(Measure measure){
+	    this.measure=measure;	
+		
+	}
+	
+	public void setIndexer(Indexer indexer){
+		this.indexer=indexer;
+	}
+	
 	/**
 	 * Execute each comparison in its own thread.
 	 * 
@@ -49,14 +75,51 @@ public class IndexingEngine {
 	 */
 	public void addDocument(File file) throws InterruptedException,
 			ExecutionException {
+			log.debug("Inside addDocument");
+			ExtractionCallable extractionCallable = new ExtractionCallable(file,
+				getAdapter(), getExtractor());
+			Future<Descriptor> descriptor = getThreadPool().submit(
+				extractionCallable);
+			getIndexer().addDescriptor(descriptor.get(),file.getName());
+			numDocs++;
+			//log.debug("Document added to index, " + file.getAbsolutePath());
+	}
+
+	public void addDocument(File file, Adapter adapter,Extractor extractor,Indexer indexer) throws InterruptedException,
+	ExecutionException {
+		log.debug("Inside addDocument");
+		ExtractionCallable extractionCallable = new ExtractionCallable(file,
+		adapter, extractor);
+		Future<Descriptor> descriptor = getThreadPool().submit(
+		extractionCallable);
+		indexer.addDescriptor(descriptor.get(),file.getName());
+		numDocs++;
+		//log.debug("Document added to index, " + file.getAbsolutePath());
+	}
+	
+	public List<SearchResult> queryIndex(File file) throws InterruptedException,ExecutionException{
+		
 		ExtractionCallable extractionCallable = new ExtractionCallable(file,
 				getAdapter(), getExtractor());
+		
 		Future<Descriptor> descriptor = getThreadPool().submit(
 				extractionCallable);
-		getIndexer().addDescriptor(descriptor.get());
-		numDocs++;
-		log.debug("Document added to index, " + file.getAbsolutePath());
+		//log.debug("Signature getIndex="+descriptor.get().toString());
+         return getIndexer().query(descriptor.get(), getMeasure());
 	}
+	
+
+public List<SearchResult> queryIndex(File file,Adapter adapter,Extractor extractor,Measure measure,Indexer indexer) throws InterruptedException,ExecutionException{
+	
+	ExtractionCallable extractionCallable = new ExtractionCallable(file,
+			adapter, extractor);
+	
+	Future<Descriptor> descriptor = getThreadPool().submit(
+			extractionCallable);
+	log.debug("Indexing Engine: queryIndex");
+	indexer.setMeasure(measure);
+	return indexer.query(descriptor.get());
+}	
 
 	public ExecutorService getThreadPool() {
 		return threadPool;
@@ -68,6 +131,9 @@ public class IndexingEngine {
 
 	public Extractor getExtractor() {
 		return extractor;
+	}
+	public Measure getMeasure(){
+		return measure;
 	}
 
 	public Indexer getIndexer() {
